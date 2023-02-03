7f0a96604f44fe442a78e21861df7d8d0837bd07
bharath <bharathprasannan96@gmail.com>
2023-02-03 12:58:40 +0530

    implemented login logout function using redux-toolkit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import LoginScreen from "./screens/LoginScreen";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseConfig";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+
+
   initializeApp(firebaseConfig);
   const auth = getAuth();
 
@@ -16,9 +22,16 @@ function App() {
     const unSubscribe = onAuthStateChanged(auth, (userAuth) => {
       if (userAuth) {
         console.log(userAuth);
+
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
+        
       } else {
         // User is signed out
         // ...
+        dispatch(logout)
       }
     });
     
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+// import { useReducer } from "../features/counter/userSlice";//
+import { userSlice } from "../features/userSlice";
+// import {userReducer} from '../features/userSlice.reducer'
+// import userReducer from './userSlice.reducer';
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+    reducer: {
+        user: userSlice.reducer,
+
+    }
+})
